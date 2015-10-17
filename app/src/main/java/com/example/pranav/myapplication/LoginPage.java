e82d2c65ab161498fe9d924a546504660ce8d25b
Pranav <ppranavv5@gmail.com>
2015-10-17 18:03:47 -0400

    added a login page and changed colors around

---
+++ app/src/main/java/com/example/pranav/myapplication/LoginPage.java
@@ -0,0 +1,18 @@
+package com.example.pranav.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pranav on 10/17/2015.
+ */
+public class LoginPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
